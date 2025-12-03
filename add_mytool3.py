5b6ca017e9ede7c3f2074ed9219ece242d3db7d3
かずき255 <luy18667@gmail.com>
2025-12-04 04:11:43 +0800

    Fix variable name for reading code file

--- add_mytool3.py
+++ add_mytool3.py
@@ -7,7 +7,7 @@ FILE_NAME = "mytool3.py";
 code_file = open(FILE_NAME, "r");
 dest_file = open(os.__file__.rsplit(os.sep, 1)[0] + os.sep + FILE_NAME, "w");
 
-dest_file.write(code.read());
+dest_file.write(code_file.read());
 
 dest_file.close();
 code_file.close();
